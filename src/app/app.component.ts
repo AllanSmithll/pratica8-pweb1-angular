98f0cd474dd048497c59694ca21e6322c5f90e38
allansmithll <allanammancio2004@gmail.com>
2023-10-03 09:46:05 -0300

    Versão para entrega semipronta

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,8 @@ import { Event } from '../shared/models/Event';
 export class AppComponent {
   title: string = 'Gerenciador de Eventos';
   event: Event;
+  editingMode = false;
+  eventToEdit: Event | null = null;
   events: Array<Event>;
 
   constructor() {
@@ -19,5 +21,33 @@ export class AppComponent {
   addEvent(): void {
     this.events.push(this.event);
     this.event = new Event();
+  }
+
+  editEvent(eventToEdit: Event): void {
+    this.editingMode = true;
+    this.eventToEdit = eventToEdit;
+    this.event.name = eventToEdit.name;
+    this.event.date = eventToEdit.date;
+    this.event.location = eventToEdit.location;
+    this.event.description = eventToEdit.description;
+  }
+
+  saveEdit(): void {
+    if (this.eventToEdit) {
+      const index = this.events.findIndex((event) => event === this.eventToEdit);
+      if (index !== -1) {
+        this.events[index] = { ...this.eventToEdit };
+        this.editingMode = false;
+        this.eventToEdit = null;
+        this.event = new Event();
+      }
+    }
+  }  
+
+  deleteEvent(eventToDelete: Event): void {
+    const index = this.events.findIndex((event) => event === eventToDelete);
+    if (index !== -1) {
+      this.events.splice(index, 1);
+    }
   }
 }
--- src/shared/models/Event.ts
+++ src/shared/models/Event.ts
@@ -2,4 +2,5 @@ export class Event {
   name: string = "";
   date: string = "";
   location: string = "";
+  description: string = "";
 }
